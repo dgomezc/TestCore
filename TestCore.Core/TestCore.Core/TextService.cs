ecd626457f9a9feb1dbd6400dd45742e2da56606
David Gómez <v-davgom@microsoft.com>
2019-05-28 11:31:41 +0200

    Change names to functions

--- TestCore.Core/TestCore.Core/TextService.cs
+++ TestCore.Core/TestCore.Core/TextService.cs
@@ -4,8 +4,8 @@ namespace TestCore.Core
 {
     public static class TextService
     {
-        public static string GetText() => "This is my new text";
+        public static string GetText2() => "This is my new text";
 
-        public static string GetNewText() => "This is my new text";
+        public static string GetNewText2() => "This is my new text";
     }
 }
